f058fdaa1e3ea439be6ecdbef338a07af4a41aee
Rauwers.Jiro <jairorauwers@gmail.com>
2023-03-07 09:53:55 -0300

    Initialize chore/AddEslint

---
+++ src/templates/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import Main from 'components/Main';
+
+import * as S from './styles';
+
+const Dashboard = () => {
+  return (
+    <S.Container>
+      <Main />
+    </S.Container>
+  );
+};
+
+export default Dashboard;
